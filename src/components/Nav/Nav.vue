9cb274bd95d661c6f6323bd284d5ca1a9da43945
Alexis Fernando Rodriguez <alexisf_27@hotmail.com>
2024-03-16 08:57:18 +0000

    Nav base

---
+++ src/components/Nav/Nav.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="parent">
+    <!-- Component content -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Nav',
+}
+</script>
+
+<style src="./nav.css" lang="css"></style>
