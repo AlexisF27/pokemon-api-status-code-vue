d37a8e5008141cdd0eea9599710d59cac8e6c73c
Alexis Fernando Rodriguez <alexisf_27@hotmail.com>
2024-03-18 18:17:26 +0000

    Nav compoent css img and logo

--- src/components/Nav/Nav.vue
+++ src/components/Nav/Nav.vue
@@ -5,6 +5,10 @@
       src="https://www.pikpng.com/pngl/b/450-4503442_transparent-background-album-on-surprised-pikachu-no-background.png"
       alt="logo"
     >
+    <div id="catch-frase-logo">
+        <h1 class="catch-frase">Gotta Catch ’Em All</h1>
+        <img id="pokemon-logo" src="../../../public/img/pokemon.png" alt="pokemon logo">
+      </div>
   </div>
 </template>
 
